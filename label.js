ad8036d26a9554c50c58cb0f17d26eed43f0d32b
Joao Falcao <joao.falcao@blip.pt>
2018-08-06 09:22:32 +0100

    add label to each follower but working because of single page app

--- label.js
+++ label.js
@@ -1,4 +1,23 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// var images = document.getElementsByTagName('img');
+// for (var i = 0, l = images.length; i < l; i++) {
+//     images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// }
+
+// var followers = document.getElementsByClassName('FPmhX');
+// for (var i = 0, l = followers.length; i < l; i++) {
+//     console.log(followers[i].text);
+//     followers[i].text = 'falcons';
+// }
+
+function addCheckBox(element) {
+    var div = document.createElement('div');
+    div.className = 'checkmark';
+    div.innerHTML =
+        '<label style="background: green; width:10px">x</label>';
+    element.appendChild(div);
+}
+
+var followers = document.getElementsByClassName('FPmhX');
+for (var i = 0, l = followers.length; i < l; i++) {
+    addCheckBox(followers[i]);
 }
